5659ac502443c7d70bf30214f6bd997b5f033df6
Austin Crane <austincrane@gmail.com>
2018-05-15 14:30:06 -0400

    add about colors

--- page-about.php
+++ page-about.php
@@ -8,25 +8,73 @@ get_header();
 get_template_part('inc/banner');
 
 ?>
-<div class="wrapper">
+
 	<div id="primary" class="content-area-full">
 		<main id="main" class="site-main" role="main">
 
 			<?php
-			while ( have_posts() ) : the_post();
+			
+			while ( have_posts() ) : the_post(); 
 
-				get_template_part( 'template-parts/content', 'page' );
+				$ourStory = get_field('our_story');
+				$why = get_field('why_you_need_a_pet_sitter');
+				$what = get_field('what_our_clients_say');
+				$how = get_field('how_can_we_help_you');
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
 
-			endwhile; // End of the loop.
 			?>
+			<div class="wrapper">	
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="entry-content">
+						<header class="entry-header">
+							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						</header><!-- .entry-header -->
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-## -->
+			</div>
+			
+			<section class="about color1">
+				<div class="wrapper">
+					<div class="entry-content">
+						<?php echo $why; ?>
+					</div>
+				</div>
+			</section>
+
+			
+			<section class="about color2">
+				<div class="wrapper">
+					<div class="entry-content">
+						<?php echo $what; ?>
+					</div>
+				</div>
+			</section>
+
+			
+			<section class="about color3">
+				<div class="wrapper">
+					<div class="entry-content">
+						<?php echo $ourStory; ?>
+					</div>
+				</div>
+			</section>
+
+			
+			<section class="about color4">
+				<div class="wrapper">
+					<div class="entry-content">
+						<?php echo $how; ?>
+					</div>
+				</div>
+			</section>
+
+
+
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-</div>
+
 <?php
 get_footer();
