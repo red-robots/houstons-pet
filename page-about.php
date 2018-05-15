f141e2d7b63ae2b6345966bba667705d16364347
Austin Crane <austincrane@gmail.com>
2018-05-15 14:09:14 -0400

    add about template

---
+++ page-about.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: About
+ */
+
+get_header(); 
+
+get_template_part('inc/banner');
+
+?>
+<div class="wrapper">
+	<div id="primary" class="content-area-full">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div>
+<?php
+get_footer();
