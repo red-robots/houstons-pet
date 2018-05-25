df3c5e787f772893531ccdf0ad85a193bc20137b
Austin Crane <austincrane@gmail.com>
2018-05-25 11:13:24 -0400

    add last 2 dos

--- archive.php
+++ archive.php
@@ -8,7 +8,7 @@
  */
 
 get_header(); ?>
-
+<div class="wrapper">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -24,28 +24,25 @@ get_header(); ?>
 
 			<?php
 			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			while ( have_posts() ) : the_post(); ?>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				<article class="blog entry-content">
+		    		<h2><?php the_title(); ?></h2>
+		    		<div class="excerpt">
+		    			<?php the_excerpt(); ?>
+		    		</div>
+		    		<div class="button">
+		    			<a href="<?php the_permalink(); ?>">READ MORE</a>
+		    		</div>
+		    	</article>
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+			<?php endwhile; ?>
+			<?php pagi_posts_nav(); ?>
+		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+	<?php get_sidebar(); ?>
+</div>
 <?php
-get_sidebar();
 get_footer();
--- footer.php
+++ footer.php
@@ -11,12 +11,15 @@
 	$phone = get_field('phone', 'option');
 	$email = get_field('email', 'option');
 	$antispam = antispambot($email);
+	$facebook = get_field('facebook', 'option');
+	$angieslist = get_field('angies_list', 'option');
 ?>
 
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="wrapper center">
+
 			<section class="footer-contact sections">
 				<div class="left">
 					<h3>CALL US</h3>
@@ -39,6 +42,24 @@
 			<section class="bottomnav">
 				<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'primary-menu' ) ); ?>
 			</section>
+			<section class="social-links">
+				<ul>
+					<?php if( $facebook ) { ?>
+					<li>
+						<a href="<?php echo $facebook; ?>">
+							<i class="fab fa-facebook-square fa-3x"></i>
+						</a>
+					</li>
+					<?php } ?>
+					<?php if( $angieslist ) { ?>
+					<li>
+						<a href="<?php echo $angieslist; ?>">
+							<img src="<?php bloginfo('template_url'); ?>/images/angies-list.png" style="width: 50px;">
+						</a>
+					</li>
+					<?php } ?>
+				</ul>
+			</section>	
 			<div class="creds">
 				© <?php echo date('Y') . ' ' . get_bloginfo('name'); ?>. All Rights Reserved. Site by <a href="https://bellaworksweb.com" target="_blank">BW</a>.
 			</div>
